f650503a55a3343d4f002ebec12c434fbc726825
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-06-12 16:48:27 +0000

    refactored to class

--- server.py
+++ server.py
@@ -1,45 +1,46 @@
 import socket
 import threading
 
-HEADER = 64
-SERVER = "192.168.1.55"
+PACKETSIZE = 64
+# SERVER = "192.168.1.55"
+SERVER = "127.0.0.1"
 PORT = 5050
-# SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!Disconnect"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+class Server:
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+    serversock = None
 
-    connected = True
-    while connected:
+    def __init__(self, server, port):
+        self.serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversock.bind((server,port))
 
-        msg = conn.recv(HEADER).decode(FORMAT)
-        if not msg:
-            continue
+    def handle_client(self, conn, addr):
+        print(f"[NEW CONNECTION] {addr} connected.")
 
-        msg_length = int(conn.recv(msg_length).decode(FORMAT))
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
+        connected = True
+        while connected:
 
-        print(f"[{addr}] {msg}")
+            msg = conn.recv(PACKETSIZE).decode(FORMAT)
 
-    conn.close()
-    
-def start():
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
+            if not msg:
+                continue
 
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn,addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() -1}")
+            print(f"[{addr}] {msg}")
 
-print("[STARTING] server is starting...")
-start()
+        conn.close()
+        
+    def start_server(self):
+        print("[STARTING] server is starting...")
+        self.serversock.listen()
+        print(f"[LISTENING] Server is listening on {SERVER}")
+
+        while True:
+            conn, addr = self.serversock.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn,addr))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.activeCount() -1}")
+
+server = Server(SERVER, PORT)
+server.start_server()
 
