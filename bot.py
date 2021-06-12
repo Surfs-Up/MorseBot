00dd29ee640e20ff237ddef9c3684cc5171b878a
Aaron Cheng <cheng.aaron@outlook.com>
2021-06-12 16:02:07 -0400

    bot ver final

--- bot.py
+++ bot.py
@@ -1,45 +1,40 @@
 import discord
 import re
 from translate import *
+from discord.ext import commands
 import config
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+bot.remove_command("help")
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
 
-
-async def get_online_users():
-    return ['abc', 'efg', 'hij', 'klm']
-
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author.bot:
         return
-    elif message.content.startswith('!help'):
-        await message.channel.send('just type lul')
-    elif message.content.startswith('!online'):
-        users = await get_online_users()
-
-        out = ""
-        for i, user in enumerate(users):
-            out += str(i) + ". " + user + "\n"
-
-        embed = discord.Embed(
-            title = "Currently Online",
-            thumbnail="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F22%2Fb7%2F3d%2F22b73ddfc4cbe22a4a6a4799bb37488b.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F797840890237780980%2F&tbnid=DYf3laN9ggHabM&vet=12ahUKEwjmiorEvZLxAhXOSawKHQ3VDq4QMygBegUIARDWAQ..i&docid=XZVKok964gm7RM&w=750&h=1000&itg=1&q=mike%20wazowski%20meme&ved=2ahUKEwjmiorEvZLxAhXOSawKHQ3VDq4QMygBegUIARDWAQ",
-            footer="https://github.com/Surfs-Up/MorseBot",
-            description=out
-        )
-
-        await message.channel.send(embed=embed)
+    if message.content.startswith("!"):
+        pass
     else:
-        if re.search('^[.-]*$', message.content):
+        if re.search('.' and '-', message.content):
             await message.channel.send(morseToEng(message.content))
         else:
             await message.channel.send(engToMorse(message.content))
-            
+    await bot.process_commands(message)
 
-    #if message.content.startswith('')
-client.run(config.TOKEN)
+@bot.command()
+async def help(ctx):
+    await ctx.channel.send('just type lul')
+
+@bot.command()
+async def link(ctx):
+    embed = discord.Embed(
+        title = "github link",
+        url = "https://github.com/Surfs-Up/MorseBot"
+        #thumbnail="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F22%2Fb7%2F3d%2F22b73ddfc4cbe22a4a6a4799bb37488b.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F797840890237780980%2F&tbnid=DYf3laN9ggHabM&vet=12ahUKEwjmiorEvZLxAhXOSawKHQ3VDq4QMygBegUIARDWAQ..i&docid=XZVKok964gm7RM&w=750&h=1000&itg=1&q=mike%20wazowski%20meme&ved=2ahUKEwjmiorEvZLxAhXOSawKHQ3VDq4QMygBegUIARDWAQ"
+    )
+    await ctx.channel.send(embed=embed)
+
+bot.run(config.TOKEN)
